b04493670c156cf8bf3b65a1f90e0fe456405e3b
geo <ravenholmgeorge@gmail.com>
2018-04-02 09:26:16 +1000

    generate a code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <random>
 
 int main()
 {
 	std::cout << "Bulls and Cows" << std::endl;
+
+	// Generate number
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	std::uniform_int_distribution<int> codegen(0,9);
+
+	int code[4] = { codegen(mt) , codegen(mt) , codegen(mt) , codegen(mt) };
+
+	std::cout << "code=" << 
+	code[0] << 
+	code[1] << 
+	code[2] << 
+	code[3] << 
+	"\n";
 
 	return 0;
 }
