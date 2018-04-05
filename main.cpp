cd74377c40a591286a3bd34af308fee4659c47d4
Geo <ravenholmgeorge@gmail.com>
2018-04-05 10:59:58 +1000

    added cow counting

--- main.cpp
+++ main.cpp
@@ -33,6 +33,7 @@ int main()
 	std::string guess;
 	int bulls,cows,toi=0;
 	char buffer[1]; // so convert to const char*
+
 	for (int i=1;i<=MAXGUESSES;++i)
 	{
 		retry:
@@ -41,26 +42,41 @@ int main()
 
 		// validate
 		if ( !validatestring(guess) ) goto retry; // try again
+		// assume from this point onward valid string
 
+		
 		bulls = 0;
 		cows = 0; // reset
 
-		// cows = matches - bulls
-		// FIND MATCHES
+		// counting table
+		int counting[10] = { 0,0,0,0,0,0,0,0,0,0 };
+		for (int k=0;k<4;++k)
+			counting[code[k]]++; // count how many of each digit in the code for cow-culation
+
+		// Calculate bulls and cows
 		for (int k=0;k<4;++k)
 		{
-			buffer[0]=guess[k];
-			toi = atoi(buffer);
-			if ( toi==code[k] ) bulls++;
-			// contains
-			//for (int c=0;c<4;++c)
-			{
+			buffer[0]=guess[k]; 
+			toi = atoi(buffer); // toi = currect number of code (char -> int)
 
+			// Bull, matching
+			if ( toi==code[k] ) 
+			{
+				bulls++;
+				counting[toi]--; // avoid cows including bulls
+				continue;
+			}
+			// Potential cow, if there are any of that digit left
+			if ( counting[toi] > 0 )
+			{
+				counting[toi]--;
+				cows++;
 			}
 			
 		}
 		// print how close
-		std::cout << "bulls: " << bulls << std::endl << std::endl;
+		std::cout << "bulls: " 	<< bulls << std::endl << std::endl;
+		std::cout << "cows: " 	<< cows << std::endl << std::endl;
 	}
 
 	return 0;
